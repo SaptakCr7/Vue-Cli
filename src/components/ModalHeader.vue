f0f3a41d74d0a20c2fc7f7efab32ffc98306cd74
Saptakcr7 <saptaksinha13898@gmail.com>
2022-05-15 08:32:05 -0700

    init

---
+++ src/components/ModalHeader.vue
@@ -0,0 +1,11 @@
+<template>
+<div class="backdrop">
+    <div class="modal">
+        <h1>Modal Title</h1>
+        <p>Modal content right Here</p>
+    </div>
+</div>    
+</template>
+
+<style scoped>
+</style >
